e271bceda4b695b12cac362b515bb8276c3e3b69
2152001 <tokaabdelgawad60@gmail.com>
2024-04-09 02:01:22 +0200

    update

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -7,19 +7,19 @@ export default function Services() {
             <div className='flex flex-row justify-between items-center h-[80%] w-[70%]'>
                 <div className='border w-[25%] h-[50%] flex justify-center items-center'>
                     <div className='flex flex-col justify-center items-center w-full h-full box'>
-                        <img src='../../public/digital-services.png' className='w-[7rem] h-[7rem] mb-[1rem]'></img>
+                        <img src='../../digital-services.png' className='w-[7rem] h-[7rem] mb-[1rem]'></img>
                         <div className='font-bold text-xl'>End-to-End <br></br> development</div>
                     </div>
                 </div>
                 <div className='border w-[25%] h-[50%] flex justify-center items-center'>
                     <div className='flex flex-col justify-center items-center w-full h-full box'>
-                        <img src='../../public/coding.png' className='w-[7rem] h-[7rem] mb-[1rem]'></img>
+                        <img src='../../coding.png' className='w-[7rem] h-[7rem] mb-[1rem]'></img>
                         <div className='font-bold text-xl text-center'>API development<br></br> & <br></br> Integration</div>
                     </div>
                 </div>
                 <div className='border w-[25%] h-[50%] flex justify-center items-center'>
                     <div className='flex flex-col justify-center items-center w-full h-full box'>
-                        <img src='../../public/web-services.png' className='w-[7rem] h-[7rem] mb-[1rem]'></img>
+                        <img src='../../web-services.png' className='w-[7rem] h-[7rem] mb-[1rem]'></img>
                         <div className='font-bold text-xl text-center'>Depolyment<br></br>  & <br></br>Maintenance</div>
                     </div>
                 </div>
